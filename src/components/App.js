67679859e3e7f06bc4365e31418032b3e3202aab
Tomasz Matras Work <tomasz.matras@developerweb.pl>
2020-06-10 14:26:55 +0200

    add basic viev of component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { Component } from "react";
+
+import StartViev from "./StartViev"
+
+const numbersCol = [
+ 
+  {id:2,
+    name:2},
+    {id:3,
+      name:3},
+      {id:4,
+        name:4},
+        {id:5,
+          name:5},
+          {id:6,
+            name:6},
+            {id:7,
+              name:7},
+              {id:8,
+                name:8},
+                {id:9,
+                  name:9},
+                  {id:10,
+                    name:10},
+]
+const numbersRow = [
+  {id:1,
+    name:1},
+{id:2,
+  name:2},
+  {id:3,
+    name:3},
+    {id:4,
+      name:4},
+      {id:5,
+        name:5},
+        {id:6,
+          name:6},
+          {id:7,
+            name:7},
+            {id:8,
+              name:8},
+              {id:9,
+                name:9},
+                {id:10,
+                  name:10},
+]
+
+class App extends Component {
+  state = {  }
 
 
-function App() {
-  return (
-    <div>Start</div>
-  );
+  render() { 
+    return ( 
+    <>
+    <div>TEst</div>
+    <StartViev numbersCol={numbersCol} numbersRow={numbersRow}/>
+    </>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/StartViev.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import "./Start.css"
+
+
+const StartViev = (props) => {
+
+
+return(
+    <div className="basicViev">
+        <div className="row">{props.numbersRow.map(number=> {
+        
+        return <div>{number.name}</div>
+        
+         })}</div>
+         <div className="column">{props.numbersCol.map(number=> {
+        
+        return <div>{number.name}</div>
+        
+         })}</div>
+        
+    
+        <div className="calculationViev"></div>
+    </div>
+)
+}
+
+export default StartViev
