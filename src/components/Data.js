85b01a9f0c469b5761859a4ea926950909661511
Tomek Matras <tomasz.matras@developerweb.pl>
2020-06-13 09:53:45 +0200

    add buttons

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./Data.css";
 const Data = (props) => {
   return (
-    <div className="data">
-      <div className="firstNo">10</div>
-      <div className="multiplication">*</div>
-      <div className="secondNo">10</div>
-      <div className="sumSign">=</div>
-      <div className="sumOfMulti">100</div>
+    <div className="container">
+      <div className="data">
+        <div className="firstNo">10</div>
+        <div className="multiplication">*</div>
+        <div className="secondNo">10</div>
+        <div className="sumSign">=</div>
+        <div className="sumOfMulti">100</div>
+      </div>
+      <div className="buttons">
+        <button className="btnDraw">Losuj</button>
+        <button className="btnCheck">Sprawdź</button>
+      </div>
     </div>
   );
 };
