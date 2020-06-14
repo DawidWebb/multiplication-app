7fae03b5332ddba1277d8a1bbaabfd842da5215a
Tomek Matras <tomasz.matras@developerweb.pl>
2020-06-14 09:40:32 +0200

    add fn cdrow, change and check

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,15 +4,26 @@ const Data = (props) => {
   return (
     <div className="container">
       <div className="data">
-        <div className="firstNo">10</div>
+        <div className="firstNo">{props.numbers[0]}</div>
         <div className="multiplication">*</div>
-        <div className="secondNo">10</div>
+        <div className="secondNo">{props.numbers[1]}</div>
         <div className="sumSign">=</div>
-        <div className="sumOfMulti">100</div>
+        <div className="sumOfMulti">
+          <input
+            onChange={props.change}
+            placeholder="???"
+            type="text"
+            value={props.value}
+          />
+        </div>
       </div>
       <div className="buttons">
-        <button className="btnDraw">Losuj</button>
-        <button className="btnCheck">Sprawdź</button>
+        <button className="btnDraw" onClick={props.draw}>
+          Losuj
+        </button>
+        <button className="btnCheck" onClick={props.check}>
+          Sprawdź
+        </button>
       </div>
     </div>
   );
--- src/components/Multi.js
+++ src/components/Multi.js
@@ -3,11 +3,47 @@ import React, { Component } from "react";
 import "./Multi.css";
 import Data from "./Data";
 class Multi extends Component {
-  state = {};
+  state = {
+    value: "",
+    numbers: [],
+  };
+  handleDraw = () => {
+    let numbers = [...this.state.numbers];
+    numbers = [];
+    let drawNumber1 = Math.floor(Math.random() * 10 + 1);
+    let drawNumber2 = Math.floor(Math.random() * 10 + 1);
+    numbers.push(drawNumber1, drawNumber2);
+    this.setState({
+      numbers,
+      value: "",
+    });
+  };
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({
+      value,
+    });
+  };
+  handleCheck = () => {
+    if (
+      this.state.value * 1 ===
+      this.state.numbers[0] * this.state.numbers[1]
+    ) {
+      return console.log("WIN");
+    } else {
+      return console.log("LOSE");
+    }
+  };
   render() {
     return (
       <div className="Multi">
-        <Data />
+        <Data
+          value={this.state.value}
+          numbers={this.state.numbers}
+          draw={this.handleDraw}
+          change={this.handleChange}
+          check={this.handleCheck}
+        />
       </div>
     );
   }
